c08430410a8f7d3a63c0794842450136cdcb0755
rb <ross.baehr@gmail.com>
2016-04-09 12:54:41 -0400

    bbox

--- launcher.py
+++ launcher.py
@@ -1,8 +1,9 @@
 from PIL import ImageGrab
 from PIL import Image
-from planar import BoundingBox
+#from planar import BoundingBox
 
 # (minx,miny), (maxx, miny), (maxx, maxy), (minx, maxy)
-bbox = BoundingBox([(1175,671),(1177,671),(1177,673),(1175,673)])
+#bbox = BoundingBox([(1175,671),(1177,671),(1177,673),(1175,673)])
+img = ImageGrab.grab(bbox=(1176,1176,672,672))
 #img = ImageGrab.grab(bbox)
 print(bbox)
