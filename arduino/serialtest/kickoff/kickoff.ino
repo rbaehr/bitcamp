edc3aac5dd2ddde17b8f3bc1b1501cabd61766ba
kmonte <lazykylem@gmail.com>
2016-04-09 20:18:29 -0400

    changes to arduino code

--- arduino/serialtest/kickoff/kickoff.ino
+++ arduino/serialtest/kickoff/kickoff.ino
@@ -4,6 +4,9 @@
 #define FORBACK 5
 #define JUMPBOOST 6
 
+#define NEUTRAL 30 
+#define UP 60
+#define DOWN 0
 Servo lr;
 Servo fb;
 Servo jb;
@@ -16,7 +19,7 @@ void right() {
   
 }
 
-void for() {
+void forw() {
   
 }
 
@@ -36,20 +39,28 @@ void flip() {
   
 }
 
+void resetall(){
+  lr.write(NEUTRAL);
+  fb.write(NEUTRAL);
+  jb.write(NEUTRAL);
+}
 void setup() {
   Serial.begin(9600); 
   pinMode(13, OUTPUT);
   lr.attach(LEFTRIGHT);
   fb.attach(FORBACK);
   jb.attach(JUMPBOOST);
+  resetall();
   
 }
 
 void loop() {
+{ 
   if (Serial.available() > 0) {
-    int b = Serial.read();
-    if ( b == 1) {
-      // Ready to go
+    int b = Serial.parseInt();
+    lr.write(b);
+    jb.write(b);
+    fb.write(b);
     }
-  }
+} 
 }
